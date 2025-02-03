12e92416f69ed004faa24214ccdd139e4f9f0ba7
Gaspar Novel Porcel <gaspar@ecodeliver.tech>
2025-02-03 11:14:10 +0100

    1.7: unicafe step 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
- // calculate statistics
+
+  // calculate statistics
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good * 1 + bad * -1) / total
+  const positivePercentage = total === 0 ? 0 : (good / total) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -20,6 +25,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>total {total}</p>
+        <p>average {average.toFixed(1)}</p>
+        <p>positive {positivePercentage.toFixed(1)} %</p>
       </div>
     </div>
   )
