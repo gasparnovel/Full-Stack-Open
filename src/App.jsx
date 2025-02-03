f328d70a13dcf39f9cb9734dc1e54ce8f74f8d5a
Gaspar Novel Porcel <gaspar@ecodeliver.tech>
2025-02-03 11:21:30 +0100

    1.10: unicafe step 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import { useState } from 'react'
 import PropTypes from 'prop-types'
 
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{typeof value === 'number' ? value.toFixed(1) : value}</td>
+  </tr>
+)
+
+StatisticLine.propTypes = {
+  text: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+Button.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired
+}
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
   
@@ -13,20 +34,22 @@ const Statistics = ({ good, neutral, bad }) => {
     )
   }
   
-  const average = total === 0 ? 0 : (good * 1 + bad * -1) / total
-  const positivePercentage = total === 0 ? 0 : (good / total) * 100
+  const average = (good - bad) / total
+  const positivePercentage = (good / total) * 100
 
   return (
     <div>
-      <h2>statistics</h2>
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>total {total}</p>
-        <p>average {average.toFixed(1)}</p>
-        <p>positive {positivePercentage.toFixed(1)} %</p>
-      </div>
+      <h2>Estadísticas</h2>
+      <table>
+        <tbody>
+          <StatisticLine text="buenos" value={good} />
+          <StatisticLine text="neutrales" value={neutral} />
+          <StatisticLine text="malos" value={bad} />
+          <StatisticLine text="total" value={total} />
+          <StatisticLine text="promedio" value={average} />
+          <StatisticLine text="positivos" value={positivePercentage} />
+        </tbody>
+      </table>
     </div>
   )
 }
@@ -37,28 +60,18 @@ Statistics.propTypes = {
   bad: PropTypes.number.isRequired
 }
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-)
-
-Button.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  text: PropTypes.string.isRequired
-}
-
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <h1>give feedback</h1>
+      <h1>Dar retroalimentación</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <Button handleClick={() => setGood(good + 1)} text="bueno" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="malo" />
       </div>
       
       <Statistics good={good} neutral={neutral} bad={bad} />
