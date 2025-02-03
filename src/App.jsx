a1f18dfb028f3a7f516fa097117fd88f4951ddc4
Gaspar Novel Porcel <gaspar@ecodeliver.tech>
2025-02-03 11:13:34 +0100

    1.6: unicafe step 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+ // calculate statistics
   return (
     <div>
       <h1>give feedback</h1>
