9d5b39cd2c3d7b4d8db58c14d73dc89b5472b389
Gaspar Novel Porcel <gaspar@ecodeliver.tech>
2025-02-03 11:01:43 +0100

    1.6: unicafe step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      ...
+      code here
     </div>
   )
 }
+
+export default App
