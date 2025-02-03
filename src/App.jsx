bbdc7f525fddaa2dbd71a4ab069f004eaa2d2cff
Gaspar Novel Porcel <gaspar@ecodeliver.tech>
2025-02-03 11:05:36 +0100

    Exercise 1.5: Course Information

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,19 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+      
+      <h2>statistics</h2>
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
     </div>
   )
 }
