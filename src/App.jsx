c438d32e118a1ffe754e421cc8be36221cb3d38e
Gaspar Novel Porcel <gaspar@ecodeliver.tech>
2025-02-03 15:21:32 +0100

    Exercise 2.9: The Phonebook Step 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -13,6 +17,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -25,16 +33,30 @@ const App = () => {
     
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     setPersons(persons.concat(personObject))
     setNewName('')
     setNewNumber('')
   }
 
+  const personsToShow = searchTerm
+    ? persons.filter(person => 
+        person.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input 
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
+      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -52,9 +74,9 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map(person => 
-        <div key={person.name}>
+      <h3>Numbers</h3>
+      {personsToShow.map(person => 
+        <div key={person.id}>
           {person.name} {person.number}
         </div>
       )}
